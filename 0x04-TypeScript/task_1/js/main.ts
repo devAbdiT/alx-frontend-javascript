a7e1f45837fd09049f339c98f15be2240b899f6b
Abdi begna <abditadesse89@gmail.com>
2025-11-11 00:02:05 +0300

    03

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -27,3 +27,16 @@ const director1: Director = {
   numberOfReports: 17,
 };
 console.log(director1);
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (
+  firstName: string,
+  lastName: string
+): string => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+console.log(printTeacher("John", "Doe")); // Output: J. Doe
