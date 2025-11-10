56aa109d4de3e484a663b36ca86251bf049403b9
Abdi begna <abditadesse89@gmail.com>
2025-11-10 22:48:30 +0300

    Refactor printTeacher function to ..

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -2,12 +2,9 @@ interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-const printTeacher: printTeacherFunction = (
-  firstName: string,
-  lastName: string
-): string => {
+function printTeacher(firstName: string, lastName: string): string {
   return `${firstName.charAt(0)}. ${lastName}`;
-};
+}
 
 console.log(printTeacher("John", "Doe")); // Output: J. Doe
 
