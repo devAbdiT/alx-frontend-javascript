fcb6235501bc572ca5dee9fad8a188059b625299
Abdi begna <abditadesse89@gmail.com>
2025-11-10 22:53:52 +0300

    charvar(0) to [0]

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -3,25 +3,25 @@ interface printTeacherFunction {
 }
 
 function printTeacher(firstName: string, lastName: string): string {
-  return `${firstName.charAt(0)}. ${lastName}`;
+  return `${firstName[0]}. ${lastName}`;
 }
 
 console.log(printTeacher("John", "Doe")); // Output: J. Doe
 
-interface Teacher {
-  readonly firstName: string;
-  readonly lastName: string;
-  fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
-  location: string;
-  [key: string]: any; // Allow any additional attributes
-}
-const teacher3: Teacher = {
-  firstName: "John",
-  fullTimeEmployee: false,
-  lastName: "Doe",
-  location: "London",
-  contract: false,
-};
+// interface Teacher {
+//   readonly firstName: string;
+//   readonly lastName: string;
+//   fullTimeEmployee: boolean;
+//   yearsOfExperience?: number;
+//   location: string;
+//   [key: string]: any; // Allow any additional attributes
+// }
+// const teacher3: Teacher = {
+//   firstName: "John",
+//   fullTimeEmployee: false,
+//   lastName: "Doe",
+//   location: "London",
+//   contract: false,
+// };
 
-console.log(teacher3);
+// console.log(teacher3);
